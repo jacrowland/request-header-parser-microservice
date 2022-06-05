ac0c00fba1ba615bf58950961d3a43a7cb7a09ee
Jacob Rowland <jacob.rowland@hotmail.com>
2022-06-05 15:22:21 +1200

    gcloud setup

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-
 const PORT = 8080;
 
 const server = app.listen(PORT, () => {
@@ -9,10 +8,19 @@ const server = app.listen(PORT, () => {
     console.log('App listening at http://%s:%s', host, port);
 })
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://www.freecodecamp.org");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
+
 app.get('/api/whoami', (req, res) => {
     let ipAddress = req.header('X-Forwarded-For');
-    let acceptedLanguage = req.header('X-Forwarded-For');
+    let acceptedLanguage = req.header('Accept-Language');
     let userAgent = req.header("user-agent");
-
     res.status(200).json({ip: ipAddress, language: acceptedLanguage, software: userAgent})
 });
